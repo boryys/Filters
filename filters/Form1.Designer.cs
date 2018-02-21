f3b11f1bd2baff628afa0ec4f99939b317bcd5b9
UlaBorys <ula_borys@op.pl>
2018-02-21 22:51:14 +0100

    negative and brightness filter

---
+++ filters/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace filters
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.inversion = new System.Windows.Forms.Button();
+            this.brightness = new System.Windows.Forms.Button();
+            this.contrast = new System.Windows.Forms.Button();
+            this.blur = new System.Windows.Forms.Button();
+            this.gaussianBlur = new System.Windows.Forms.Button();
+            this.sharpen = new System.Windows.Forms.Button();
+            this.edgeDetection = new System.Windows.Forms.Button();
+            this.emboss = new System.Windows.Forms.Button();
+            this.editor = new System.Windows.Forms.Button();
+            this.original = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.BackColor = System.Drawing.Color.White;
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel.Controls.Add(this.tableLayoutPanel1, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.pictureBox, 1, 0);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 1;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(784, 512);
+            this.tableLayoutPanel.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.inversion, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.brightness, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.contrast, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.blur, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.gaussianBlur, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.sharpen, 0, 7);
+            this.tableLayoutPanel1.Controls.Add(this.edgeDetection, 0, 8);
+            this.tableLayoutPanel1.Controls.Add(this.emboss, 0, 9);
+            this.tableLayoutPanel1.Controls.Add(this.editor, 0, 10);
+            this.tableLayoutPanel1.Controls.Add(this.original, 0, 12);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 12;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692485F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692485F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692485F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692485F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692485F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692485F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692485F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692485F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692485F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692485F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692485F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692485F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.690178F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(150, 512);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // inversion
+            // 
+            this.inversion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inversion.Location = new System.Drawing.Point(5, 44);
+            this.inversion.Margin = new System.Windows.Forms.Padding(5);
+            this.inversion.Name = "inversion";
+            this.inversion.Size = new System.Drawing.Size(140, 29);
+            this.inversion.TabIndex = 0;
+            this.inversion.Text = "inversion";
+            this.inversion.UseVisualStyleBackColor = true;
+            this.inversion.Click += new System.EventHandler(this.inversion_Click);
+            // 
+            // brightness
+            // 
+            this.brightness.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.brightness.Location = new System.Drawing.Point(5, 83);
+            this.brightness.Margin = new System.Windows.Forms.Padding(5);
+            this.brightness.Name = "brightness";
+            this.brightness.Size = new System.Drawing.Size(140, 29);
+            this.brightness.TabIndex = 1;
+            this.brightness.Text = "brightness";
+            this.brightness.UseVisualStyleBackColor = true;
+            this.brightness.Click += new System.EventHandler(this.brightness_Click);
+            // 
+            // contrast
+            // 
+            this.contrast.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.contrast.Location = new System.Drawing.Point(5, 122);
+            this.contrast.Margin = new System.Windows.Forms.Padding(5);
+            this.contrast.Name = "contrast";
+            this.contrast.Size = new System.Drawing.Size(140, 29);
+            this.contrast.TabIndex = 2;
+            this.contrast.Text = "contrast";
+            this.contrast.UseVisualStyleBackColor = true;
+            // 
+            // blur
+            // 
+            this.blur.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.blur.Location = new System.Drawing.Point(3, 198);
+            this.blur.Name = "blur";
+            this.blur.Size = new System.Drawing.Size(144, 33);
+            this.blur.TabIndex = 4;
+            this.blur.Text = "blur";
+            this.blur.UseVisualStyleBackColor = true;
+            // 
+            // gaussianBlur
+            // 
+            this.gaussianBlur.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gaussianBlur.Location = new System.Drawing.Point(3, 237);
+            this.gaussianBlur.Name = "gaussianBlur";
+            this.gaussianBlur.Size = new System.Drawing.Size(144, 33);
+            this.gaussianBlur.TabIndex = 5;
+            this.gaussianBlur.Text = "gaussian blur";
+            this.gaussianBlur.UseVisualStyleBackColor = true;
+            // 
+            // sharpen
+            // 
+            this.sharpen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sharpen.Location = new System.Drawing.Point(3, 276);
+            this.sharpen.Name = "sharpen";
+            this.sharpen.Size = new System.Drawing.Size(144, 33);
+            this.sharpen.TabIndex = 6;
+            this.sharpen.Text = "sharpen";
+            this.sharpen.UseVisualStyleBackColor = true;
+            // 
+            // edgeDetection
+            // 
+            this.edgeDetection.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.edgeDetection.Location = new System.Drawing.Point(3, 315);
+            this.edgeDetection.Name = "edgeDetection";
+            this.edgeDetection.Size = new System.Drawing.Size(144, 33);
+            this.edgeDetection.TabIndex = 7;
+            this.edgeDetection.Text = "edge detection";
+            this.edgeDetection.UseVisualStyleBackColor = true;
+            // 
+            // emboss
+            // 
+            this.emboss.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.emboss.Location = new System.Drawing.Point(3, 354);
+            this.emboss.Name = "emboss";
+            this.emboss.Size = new System.Drawing.Size(144, 33);
+            this.emboss.TabIndex = 8;
+            this.emboss.Text = "emboss";
+            this.emboss.UseVisualStyleBackColor = true;
+            // 
+            // editor
+            // 
+            this.editor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.editor.Location = new System.Drawing.Point(3, 393);
+            this.editor.Name = "editor";
+            this.editor.Size = new System.Drawing.Size(144, 33);
+            this.editor.TabIndex = 9;
+            this.editor.Text = "editor";
+            this.editor.UseVisualStyleBackColor = true;
+            // 
+            // original
+            // 
+            this.original.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.original.Location = new System.Drawing.Point(5, 473);
+            this.original.Margin = new System.Windows.Forms.Padding(5);
+            this.original.Name = "original";
+            this.original.Size = new System.Drawing.Size(140, 34);
+            this.original.TabIndex = 10;
+            this.original.Text = "original";
+            this.original.UseVisualStyleBackColor = true;
+            this.original.Click += new System.EventHandler(this.original_Click);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 39);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Function filters";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 39);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Convolution filters";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(153, 3);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(628, 506);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(784, 512);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 550);
+            this.MinimumSize = new System.Drawing.Size(800, 550);
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Filters";
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button inversion;
+        private System.Windows.Forms.Button brightness;
+        private System.Windows.Forms.Button contrast;
+        private System.Windows.Forms.Button blur;
+        private System.Windows.Forms.Button gaussianBlur;
+        private System.Windows.Forms.Button sharpen;
+        private System.Windows.Forms.Button edgeDetection;
+        private System.Windows.Forms.Button emboss;
+        private System.Windows.Forms.Button editor;
+        private System.Windows.Forms.Button original;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ filters/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace filters
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        Bitmap originalPhoto;
+
+        int bc = 70;
+
+        public Form()
+        {
+            InitializeComponent();
+
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            originalPhoto = Properties.Resources.Penguins;
+            pictureBox.Image = originalPhoto;
+        }
+
+        private void inversion_Click(object sender, EventArgs e)
+        {
+            Color color;
+            int r, g, b;
+            Bitmap tmp = (Bitmap)originalPhoto.Clone();
+
+            for (int x = 0; x < originalPhoto.Width; x++)
+            {
+                for(int y = 0; y < originalPhoto.Height; y++)
+                {
+                    color = originalPhoto.GetPixel(x, y);
+
+                    r = 255 - color.R;
+                    g = 255 - color.G;
+                    b = 255 - color.B;
+
+                    tmp.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+
+            pictureBox.Image = tmp;
+        }
+
+        private void brightness_Click(object sender, EventArgs e)
+        {
+            Color color;
+            int r, g, b;
+            Bitmap tmp = (Bitmap)originalPhoto.Clone();
+
+            for (int x = 0; x < originalPhoto.Width; x++)
+            {
+                for (int y = 0; y < originalPhoto.Height; y++)
+                {
+                    color = originalPhoto.GetPixel(x, y);
+
+                    if (color.R + bc > 255) r = 255;
+                    else r = color.R + bc;
+
+                    if (color.G + bc > 255) g = 255;
+                    else g = color.G + bc;
+
+                    if (color.B + bc > 255) b = 255;
+                    else b = color.B + bc;
+
+                    tmp.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+
+            pictureBox.Image = tmp;
+        }
+
+        private void original_Click(object sender, EventArgs e)
+        {
+            pictureBox.Image = originalPhoto;
+        }
+    }
+}
